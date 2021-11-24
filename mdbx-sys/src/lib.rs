3e54489d99961869b5ab2c275fd6134936a4a38b
gcxfd <i@rmw.link>
2021-11-24 16:59:39 +0800

    🔶

--- mdbx-sys/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![deny(warnings)]
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(clippy::all)]
-#![doc(html_root_url = "https://docs.rs/mdbx-sys/0.9.3")]
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
